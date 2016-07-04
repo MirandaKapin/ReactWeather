081d18b762c63c8a10f9e2d8528b21479e1f21ed
Miranda Kapin <mirandakapin@gmail.com>
2016-07-04 12:32:17 -0700

    Added comments to server.js about http redirect

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use (function (req, res, next) {
+// Redirect all traffic to http traffic
+app.use(function (req, res, next) {
 	if (req.headers['x-forwarded-proto'] === 'http') {
 		next();
 	} else {
